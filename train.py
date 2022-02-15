baa3128b32b98ac4a51d2d916911e5394b2a3f93
Khalil Mrini <KhalilMrini@users.noreply.github.com>
2022-02-15 22:01:33 +0100

    Update train.py

--- train.py
+++ train.py
@@ -602,8 +602,10 @@ class Modal(object):
                     pv_turn['resp'] = turn['resp'] if cfg.use_true_prev_resp else decoded['resp']
                     pv_turn['bspn'] = turn['bspn'] if cfg.use_true_prev_bspn else decoded['bspn']
                     pv_turn['db'] = turn['db'] if cfg.use_true_curr_bspn else db
-                    if cfg.use_true_curr_tspn or cfg.predict_turn_number or (cfg.turn_number_threshold > 0 and turn_idx == dialog_len - cfg.turn_number_threshold):
-                        pv_turn['tspn'] = turn['tspn'] if cfg.use_true_curr_tspn or (cfg.turn_number_threshold > 0 and turn_idx == dialog_len - cfg.turn_number_threshold) else decoded['tspn']
+                    if cfg.use_true_curr_tspn or cfg.predict_turn_number:
+                        pv_turn['tspn'] = turn['tspn'] if cfg.use_true_curr_tspn else decoded['tspn']
+                    if (cfg.turn_number_threshold > 0 and turn_idx == dialog_len - cfg.turn_number_threshold):
+                        pv_turn['tspn'] = ''
                     pv_turn['aspn'] = turn['aspn'] if cfg.use_true_prev_aspn else decoded['aspn']
 
                 result_collection.update(
