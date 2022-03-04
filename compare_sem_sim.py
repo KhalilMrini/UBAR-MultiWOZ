f687f0d4de7e3545c495fc7229d082917b5e40ec
Khalil Mrini <KhalilMrini@users.noreply.github.com>
2022-03-04 19:11:22 +0100

    Update compare_sem_sim.py

--- compare_sem_sim.py
+++ compare_sem_sim.py
@@ -25,8 +25,8 @@ for batch_idx, dial_batch in tqdm(enumerate(data_iterator)):
             if smallest_idx is None or this_turn_idx < smallest_idx:
                 smallest_idx = this_turn_idx
             with torch.no_grad():
-                usr_vec = model(torch.tensor(turn_text['user']))[0]
-                resp_vec = model(torch.tensor(turn_text['resp']))[0]
+                usr_vec = model(torch.tensor(turn_text['user']).cuda())[0]
+                resp_vec = model(torch.tensor(turn_text['resp']).cuda())[0]
                 cos_sim = cosine_similarity(usr_vec.unsqueeze(-1), resp_vec.transpose(0,1).unsqueeze(0), dim=1)
                 rbert = cos_sim.max(dim=1)[0].mean()
                 pbert = cos_sim.max(dim=0)[0].mean()
